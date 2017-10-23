4c3b5964d87355ecc06d59e221469a0249d238d8
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2017-10-23 17:22:26 +0100

    Update function and add comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 export default function rainDropSpeaks(arg) {
+  // check for edge case
   if (!Number.isInteger(arg)) return 'Argument must be an integer';
   if (arg < 1) return 'Argument must be greater 0';
 
   let outputString = '';
-
   const primeFactors = {
     3: 'Pling',
     5: 'Plang',
@@ -12,7 +12,7 @@ export default function rainDropSpeaks(arg) {
 
   Object.keys(primeFactors)
     .forEach((key) => {
-      if (!(arg % key)) {
+      if (!(arg % key)) { // if input arg is divisible by the key
         outputString += primeFactors[key];
       }
     });
